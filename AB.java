c2d818ceea0bae13d15c19a83209f2d70c0a0443
CHANDU <gopichandnaidu65@gmail.com>
2023-09-30 11:18:06 +0530

    b

---
+++ AB.java
@@ -0,0 +1,12 @@
+public class AB {
+    public int getab(int a ,int b){
+        int ab=a*a+b*b+2*a*b;
+        return ab;
+    }
+        public static void main(String[]args){
+        AB ab=new AB();
+        System.out.println(ab.getab(2,3));
+
+        }
+
+}

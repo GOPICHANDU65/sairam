054700cd0a66338be9dd05b43ab83c70bae4577a
CHANDU <gopichandnaidu65@gmail.com>
2023-10-05 20:04:30 +0530

    dd

---
+++ Son.java
@@ -0,0 +1,8 @@
+public class Son extends Father {
+    public Son (){
+        System.out.println("Cricket");
+    }
+    public static void main(String[]args){
+        Son chandu = new Son();
+    }
+}

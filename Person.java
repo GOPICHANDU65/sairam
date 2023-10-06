730d735d6695a3b9f1b18509bbc782a2ec5ea2e8
CHANDU <gopichandnaidu65@gmail.com>
2023-10-06 15:41:46 +0530

    ddd

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private int adhno;
+    public Person(int adhno){
+        this.adhno=adhno;
+    }
+    public static void main(String[]args){
+        Person pp = new Person(788);
+        Person ppp= new Person(789);
+        System.out.println(pp.adhno);
+        System.out.println(ppp.adhno);
+    }
+
+
+
+}

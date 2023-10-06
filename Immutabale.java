63eb8c788b89c4d345278103d5d7564bcc764039
CHANDU <gopichandnaidu65@gmail.com>
2023-10-06 16:12:50 +0530

    immutabale class

---
+++ Immutabale.java
@@ -0,0 +1,11 @@
+public class Immutabale {
+
+    public static void main(String[]args){
+        Person pp = new Person(100);
+        Person ppp = new Person(101);
+        System.out.println(pp.getperson());
+        System.out.println(ppp.getperson());
+    }
+
+
+}
